856f2331f9143500f88e61025dce71bdb41cd5fc
Baljeet Kumar <baljeet.kumar@razorpay.com>
2020-01-13 15:21:22 +0530

    added golang config looking at https://arenzana.org/2019/12/emacs-go-mode-revisited/

--- init.el
+++ init.el
@@ -25,7 +25,7 @@
  '(helm-truncate-lines t)
  '(package-selected-packages
    (quote
-    (rjsx-mode emmet-mode magit neotree web-mode yasnippet-snippets vue-mode vue-html-mode mmm-mode company-tern xref-js2 js2-refactor js2-mode helm-projectile projectile doom-modeline doom-themes color-theme-solarized cherry-blossom-theme berrys-theme whole-line-or-region smartparens use-package idle-org-agenda counsel-org-clock counsel-org-capture-string org-protocol-jekyll orgnav orca org-drill org-pomodoro org-clock-today org-brain org-bullets org-plus-contrib)))
+    (go-mode flycheck company-lsp lsp-ui lsp-mode rjsx-mode emmet-mode magit neotree web-mode yasnippet-snippets vue-mode vue-html-mode mmm-mode company-tern xref-js2 js2-refactor js2-mode helm-projectile projectile doom-modeline doom-themes color-theme-solarized cherry-blossom-theme berrys-theme whole-line-or-region smartparens use-package idle-org-agenda counsel-org-clock counsel-org-capture-string org-protocol-jekyll orgnav orca org-drill org-pomodoro org-clock-today org-brain org-bullets org-plus-contrib)))
  '(projectile-completion-system (quote helm))
  '(tool-bar-mode nil))
 (require 'helm-config)
@@ -127,6 +127,91 @@
 
 (require 'rjsx-mode)
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . rjsx-mode))
+
+(setq lsp-gopls-staticcheck t)
+(setq lsp-eldoc-render-all t)
+(setq lsp-gopls-complete-unimported t)
+
+(use-package lsp-mode
+  :ensure t
+  :commands (lsp lsp-deferred)
+  :hook (go-mode . lsp-deferred))
+
+;;Set up before-save hooks to format buffer and add/delete imports.
+;;Make sure you don't have other gofmt/goimports hooks enabled.
+
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+;;Optional - provides fancier overlays.
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode
+  :init
+)
+
+;;Company mode is a standard completion package that works well with lsp-mode.
+;;company-lsp integrates company mode completion with lsp-mode.
+;;completion-at-point also works out of the box but doesn't support snippets.
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0)
+  (setq company-minimum-prefix-length 1))
+
+(use-package company-lsp
+  :ensure t
+  :commands company-lsp)
+
+;;Optional - provides snippet support.
+
+(use-package yasnippet
+  :ensure t
+  :commands yas-minor-mode
+  :hook (go-mode . yas-minor-mode))
+
+;;lsp-ui-doc-enable is false because I don't like the popover that shows up on the right
+;;I'll change it if I want it back
+
+
+(setq lsp-ui-doc-enable nil
+      lsp-ui-peek-enable t
+      lsp-ui-sideline-enable t
+      lsp-ui-imenu-enable t
+      lsp-ui-flycheck-enable t)
+
+(defun custom-go-mode ()
+  (display-line-numbers-mode 1))
+
+(use-package go-mode
+:defer t
+:ensure t
+:mode ("\\.go\\'" . go-mode)
+:init
+  (setq compile-command "echo Building... && go build -v && echo Testing... && go test -v && echo Linter... && golint")  
+  (setq compilation-read-command nil)
+  (add-hook 'go-mode-hook 'custom-go-mode)
+:bind (("M-," . compile)
+("M-." . godef-jump)))
+
+(setq compilation-window-height 14)
+(defun my-compilation-hook ()
+  (when (not (get-buffer-window "*compilation*"))
+    (save-selected-window
+      (save-excursion
+	(let* ((w (split-window-vertically))
+	       (h (window-height w)))
+	  (select-window w)
+	  (switch-to-buffer "*compilation*")
+	  (shrink-window (- h compilation-window-height)))))))
+(add-hook 'compilation-mode-hook 'my-compilation-hook)
+
+(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
+(setq compilation-scroll-output t)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
